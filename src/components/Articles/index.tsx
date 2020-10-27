3a589cf6e71250696e6c14a027193fb7a581373c
XiaoxiaYao <michaelyaonz@gmail.com>
2020-10-27 10:48:02 +0800

    Load data

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+interface Article {
+  userId: string;
+  id: string;
+  title: string;
+  body: string;
+}
 
 const Articles = () => {
+  const [articles, setArticles] = useState(null as [Article] | null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const result = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await result.json();
+      setArticles(data);
+    }, 5000);
+  }, []);
+
   return (
     <div className="articles">
       <h2>Articles</h2>
+      {articles &&
+        articles.map(article => (
+          <div className="article" key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        ))}
+      {!articles && <div>Loading...</div>}
     </div>
   );
 };
--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+interface Profile {
+  username: string;
+  email: string;
+  website: string;
+}
 
 const User = () => {
+  const [profile, setProfile] = useState(null as null | Profile);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const result = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      const data = await result.json();
+      setProfile(data);
+    }, 5000);
+  }, []);
+
   return (
     <div className="user">
       <h2>User</h2>
+      {profile && (
+        <div className="profile">
+          <h3>{profile.username}</h3>
+          <p>{profile.email}</p>
+          <a href={profile.website}>{profile.website}</a>
+        </div>
+      )}
+      {!profile && <div>Loading...</div>}
     </div>
   );
 };
